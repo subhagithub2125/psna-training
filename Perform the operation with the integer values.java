f4b175d7b8adc766528f0d83ec84bb06f365aad3
subhagithub2125 <162798321+subhagithub2125@users.noreply.github.com>
2025-08-02 20:53:09 +0530

    Create Perform the operation with the integer values.java

---
+++ Perform the operation with the integer values.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+
+        
+        int num1 = sc.nextInt();
+        char op = sc.next().charAt(0);
+        int num2 = sc.nextInt();
+
+        int result = 0;
+
+        
+        if (op == '+') {
+            result = num1 + num2;
+        } else if (op == '-') {
+            result = num1 - num2;
+        } else if (op == '*') {
+            result = num1 * num2;
+        } else if (op == '/') {
+            result = num1 / num2;  
+        }
+
+        System.out.println(result);
+    }
+}
