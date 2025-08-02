7b89f570c1d119f97de5dc2889ef8d6041debc78
subhagithub2125 <162798321+subhagithub2125@users.noreply.github.com>
2025-08-02 20:51:05 +0530

    Create Check whether a triangle is Equilateral or Isosceles or Scalene based on length of three sides.java

---
+++ Check whether a triangle is Equilateral or Isosceles or Scalene based on length of three sides.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+       
+        if (a == b && b == c) {
+            System.out.println("Equilateral");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
+        }
+    }
+}
