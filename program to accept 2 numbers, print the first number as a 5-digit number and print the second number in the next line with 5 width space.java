c1a34b20ddea9f95a182ba48d27a2fbf957021b2
subhagithub2125 <162798321+subhagithub2125@users.noreply.github.com>
2025-08-02 21:11:13 +0530

    Create program to accept 2 numbers, print the first number as a 5-digit number and print the second number in the next line with 5 width space.java

---
+++ program to accept 2 numbers, print the first number as a 5-digit number and print the second number in the next line with 5 width space.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.printf("%05d\n",a);
+        System.out.printf("%5d\n",b);
+
+        
+    }
+}
