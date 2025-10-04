2ab61dffb7f566e43a864da245a917d2891c367f
subhagithub2125 <162798321+subhagithub2125@users.noreply.github.com>
2025-10-04 22:00:08 +0530

    Create Print all the factors of the given number.java

---
+++ Print all the factors of the given number.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n/2; i++){
+            if(n%i == 0)
+                System.out.print(i + ",");
+        }
+        System.out.print(n);
+    }
+}
